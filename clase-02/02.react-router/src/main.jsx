79b18619ea9287ed92b4ff91a408209ee9197577
Daniel <danielgeier@hotmail.com>
2026-07-02 17:31:40 -0300

    Trabajamos con router e incorporamos el footer y navbar

--- clase-02/02.react-router/src/main.jsx
+++ clase-02/02.react-router/src/main.jsx
@@ -2,9 +2,34 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
+import { BrowserRouter, Route, Routes } from 'react-router'
+import Productos from './pages/Productos.jsx'
+import Servicios from './pages/Servicios.jsx'
+import Nosotros from './pages/Nosotros.jsx'
+import Contacto from './pages/Contacto.jsx'
+import NoEncontrado from './pages/NoEncontrado.jsx'
+import Contenedor from './components/Contenedor.jsx'
+import NavBar from './components/NavBar.jsx'
+import Footer from './components/Footer.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <Contenedor>
+      <BrowserRouter>
+
+        <NavBar />
+        <Routes>
+          <Route path='/' element={<App />}/> {/* switch */}
+          <Route path='/productos' element={<Productos />} /> {/* case */}
+          <Route path='/servicios' element={<Servicios/>} />{/* case */}
+          <Route path='/nosotros' element={<Nosotros/>} />{/* case */}
+          <Route path='contacto' element={<Contacto/>} /> {/* case */}
+          <Route path='*' element={<NoEncontrado/>} /> {/* default */}
+        </Routes>
+
+        <Footer />
+
+      </BrowserRouter>
+    </Contenedor>
   </StrictMode>,
 )
